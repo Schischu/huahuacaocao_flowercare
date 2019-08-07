9538a4a04f95851d61fd252c9e467879c2fb920e
Schischu <schischu65@gmail.com>
2019-08-07 21:56:40 +0100

    Change influxdb prefix

--- mifloraPrometheus.py
+++ mifloraPrometheus.py
@@ -1,8 +1,10 @@
 import sys
 import json
-from prometheus_client import CollectorRegistry, Gauge, Summary, Enum, push_to_gateway
 import time
 from datetime import datetime
+
+from prometheus_client import CollectorRegistry, Gauge, Summary, Enum, push_to_gateway
+from influxdb import InfluxDBClient
 
 from miflora import Miflora, MifloraScanner
 
@@ -23,11 +25,40 @@ def main(argv):
   if configuration.has_key("prometheuspush-prefix") is False:
     configuration["prometheuspush-prefix"] = "flower"
 
+  if configuration.has_key("influxdb-client") is False:
+    configuration["influxdb-client"] = "Ruuvi-Influxdb"
+
+  if configuration.has_key("influxdb-server") is False:
+    configuration["influxdb-server"] = "127.0.0.1"
+
+  if configuration.has_key("influxdb-username") is False:
+    configuration["influxdb-username"] = "influxdb"
+
+  if configuration.has_key("influxdb-password") is False:
+    configuration["influxdb-password"] = "influxdb"
+
+  if configuration.has_key("influxdb-port") is False:
+    configuration["influxdb-port"] = 8086
+
+  if configuration.has_key("influxdb-database") is False:
+    configuration["influxdb-database"] = "measurements"
+
+  if configuration.has_key("influxdb-prefix") is False:
+    configuration["influxdb-prefix"] = "sensor.flower"
+
   print "Configuration:"
   print "Prometheus Push Client:   ", configuration["prometheuspush-client"]
   print "Prometheus Push Server:   ", configuration["prometheuspush-server"]
   print "Prometheus Push Port:     ", configuration["prometheuspush-port"]
   print "Prometheus Push Prefix   :", configuration["prometheuspush-prefix"]
+
+  print "Influxdb Push Client:     ", configuration["influxdb-client"]
+  print "Influxdb Push Username:   ", configuration["influxdb-username"]
+  print "Influxdb Push Password:   ", configuration["influxdb-password"]
+  print "Influxdb Push Server:     ", configuration["influxdb-server"]
+  print "Influxdb Push Port:       ", configuration["influxdb-port"]
+  print "Influxdb Push Database    ", configuration["influxdb-database"]
+  print "Influxdb Push Prefix      ", configuration["influxdb-prefix"]
 
   plants = []
   sensors = []
@@ -45,6 +76,11 @@ def main(argv):
   scanner = MifloraScanner()
   devices = scanner.discoverAll()
 
+  influxDbClient = InfluxDBClient(configuration["influxdb-server"], configuration["influxdb-port"], 
+    configuration["influxdb-username"], configuration["influxdb-password"], configuration["influxdb-database"])
+
+  influxDbClient.create_database(configuration["influxdb-database"])
+
   for device in devices:
     print device
 
@@ -54,7 +90,7 @@ def main(argv):
         deviceSensor = sensor
 
     print "deviceSensor", deviceSensor
-    sensorId = deviceSensor["name"][-4:].lower()
+    sensorId = str(deviceSensor["name"][-4:].lower())
 
     devicePlant = None
     if deviceSensor is not None and deviceSensor.has_key("plant-name"):
@@ -70,7 +106,7 @@ def main(argv):
       for i in range(0,10):
         if eventData is not None:
           #print "eventData", eventData
-          dataToPrometheus(sensorId, eventData.battery, eventData, configuration, plant)
+          dataToPrometheus(sensorId, eventData.battery, eventData, configuration, plant, influxDbClient)
           eventData = None
           observed = observed + 1
 
@@ -89,11 +125,11 @@ def main(argv):
         battery =  device.getBattery()
         realtimeData = device.getRealtimeData()
 
-        dataToPrometheus(sensorId, battery, realtimeData, configuration, plant)
+        dataToPrometheus(sensorId, battery, realtimeData, configuration, plant, influxDbClient)
 
         time.sleep(0.2)
 
-def dataToPrometheus(sensorId, battery, realtimeData, configuration, plant):
+def dataToPrometheus(sensorId, battery, realtimeData, configuration, plant, influxDbClient):
   flower = {}
 
   #flower["plant_name"] = ("Plant", devicePlant["name"])
@@ -102,10 +138,10 @@ def dataToPrometheus(sensorId, battery, realtimeData, configuration, plant):
     flower["battery"] = ("Battery", battery)
 
   if realtimeData.battery is not None:
-    flower["battery"] = ("Battery", realtimeData.battery)
+    flower["battery"] = ("Battery", int(realtimeData.battery))
 
   if realtimeData.temperature is not None:
-    flower["air_temperature"] = ("Temperature", realtimeData.temperature)
+    flower["air_temperature"] = ("Temperature", float(realtimeData.temperature))
     flower["air_temperature_status"] = ["Temperature Status", "good", ["good", "too_low", "too_high"]]
 
     if realtimeData.temperature < plant["temperature_C_threshold_lower"]:
@@ -114,7 +150,7 @@ def dataToPrometheus(sensorId, battery, realtimeData, configuration, plant):
       flower["air_temperature_status"][1] = "too_high"
 
   if realtimeData.conductivity is not None:
-    flower["fertilizer"] = ("Fertilizer", realtimeData.conductivity)
+    flower["fertilizer"] = ("Fertilizer", int(realtimeData.conductivity))
     flower["fertilizer_status"] = ["Fertilizer Status", "good", ["good", "too_low", "too_high"]]
 
     if realtimeData.conductivity < plant["fertility_us_cm_threshold_lower"]:
@@ -123,7 +159,7 @@ def dataToPrometheus(sensorId, battery, realtimeData, configuration, plant):
       flower["fertilizer_status"][1] = "too_high"
 
   if realtimeData.light is not None:
-    flower["light"] = ("Light", realtimeData.light)
+    flower["light"] = ("Light", int(realtimeData.light))
     flower["light_status"] = ["Light Status", "good", ["good", "too_low", "too_high"]]
 
     if realtimeData.light < plant["light_lux_threshold_lower"]:
@@ -132,7 +168,7 @@ def dataToPrometheus(sensorId, battery, realtimeData, configuration, plant):
       flower["light_status"][1] = "too_high"
 
   if realtimeData.moisture is not None:
-    flower["watering"] = ("Moisture", realtimeData.moisture)
+    flower["watering"] = ("Moisture", int(realtimeData.moisture))
     flower["watering_status"] = ["Moisture Status", "good", ["good", "too_low", "too_high"]]
 
     if realtimeData.moisture < plant["moisture_threshold_lower"]:
@@ -140,8 +176,8 @@ def dataToPrometheus(sensorId, battery, realtimeData, configuration, plant):
     elif realtimeData.moisture > plant["moisture_threshold_upper"]:
       flower["watering_status"][1] = "too_high"
 
-  #now = datetime.utcnow()
-  #flower["last_utc"] = ("Updated", now.strftime("%Y-%m-%dT%H:%M:%SZ")) #2017-11-13T17:44:11Z
+  now = datetime.utcnow()
+  lastUtc = ("Updated", now.strftime("%Y-%m-%dT%H:%M:%SZ")) #2017-11-13T17:44:11Z
 
   registry = CollectorRegistry()
   for key in flower.keys():
@@ -165,6 +201,22 @@ def dataToPrometheus(sensorId, battery, realtimeData, configuration, plant):
   push_to_gateway(configuration["prometheuspush-server"] + ":" + configuration["prometheuspush-port"], 
     job=configuration["prometheuspush-client"] + "_" + sensorId, 
     registry=registry)
+
+  influxDbJson = [
+  {
+    "measurement": configuration["influxdb-prefix"],
+    "tags": {
+        "sensor": sensorId,
+    },
+    "time": lastUtc[1],
+    "fields": {
+    }
+  }]
+  for key in flower.keys():
+    influxDbJson[0]["fields"][key] = flower[key][1]
+
+  print "Pushing", influxDbJson
+  influxDbClient.write_points(influxDbJson)
 
 if __name__ == "__main__":
   main(sys.argv)
